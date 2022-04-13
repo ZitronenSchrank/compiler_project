9149f46a04aea5ef1df072333f1f6cc36e7266a4
Anton Petrov <petrov.anton@web.de>
2022-04-13 20:43:12 +0200

    Added Compiler Maven Project with Java ASM

---
+++ compiler/src/main/java/de/hfu/App.java
@@ -0,0 +1,65 @@
+package de.hfu;
+
+import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
+import static org.objectweb.asm.Opcodes.ACC_STATIC;
+import static org.objectweb.asm.Opcodes.ACC_SUPER;
+import static org.objectweb.asm.Opcodes.GETSTATIC;
+import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
+import static org.objectweb.asm.Opcodes.V1_8;
+
+import static java.nio.file.StandardOpenOption.*;
+import java.nio.file.*;
+import java.io.*;
+
+import static org.objectweb.asm.Opcodes.RETURN;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public App() {
+        ClassWriter classWriter = new ClassWriter(0);
+
+        classWriter.visit(
+                V1_8, // Java 1.8
+                ACC_PUBLIC + ACC_SUPER, // public static
+                "Hello", // Class Name
+                null, // Generics <T>
+                "java/lang/Object", // Interface extends Object (Super Class),
+                null // interface names
+        );
+
+        MethodVisitor mainMethodVisitor = classWriter.visitMethod(ACC_PUBLIC + ACC_STATIC, "main",
+                "([Ljava/lang/String;)V", null, null);
+
+        mainMethodVisitor.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+        mainMethodVisitor.visitLdcInsn("Hello World");
+        mainMethodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V",
+                false);
+        mainMethodVisitor.visitInsn(RETURN);
+        mainMethodVisitor.visitEnd();
+        mainMethodVisitor.visitMaxs(2, 1);
+        classWriter.visitEnd();
+
+        try {
+            Path p = Paths.get("./Hello.class");
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(p, CREATE, WRITE, TRUNCATE_EXISTING));
+            out.write(classWriter.toByteArray());
+            out.flush();
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new App();
+    }
+}
