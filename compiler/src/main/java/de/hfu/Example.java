afc1f16923bd5fcf2b3d3ff5505c87692003c01e
Anton Petrov <petrov.anton@web.de>
2022-04-26 22:46:09 +0200

    Renamend and added more comments

--- compiler/src/main/java/de/hfu/Example.java
+++ compiler/src/main/java/de/hfu/Example.java
@@ -3,22 +3,36 @@ package de.hfu;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Test {
+public class Example {
 
+    // Implementierung von Pred()
     public static BigInteger pred(BigInteger s1) {
         BigInteger retVal = s1.add(BigInteger.ZERO); // Copy S1
         retVal = retVal.subtract(BigInteger.ONE);
 
+        // Wert darf nie kleiner 0 werden
         if (retVal.compareTo(BigInteger.ZERO) == -1) {
             retVal = BigInteger.ZERO;
         }
         return retVal;
     }
 
+    // Implementierung von succ()
     public static BigInteger succ(BigInteger s1) {
         return s1.add(BigInteger.ONE);
     }
 
+    // Implementierung von read()
+    public static BigInteger read(String name) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(name + " := ");
+        String input = in.nextLine();
+        in.close();
+        return new BigInteger(input);
+    }
+
+    // Eigene Funktion add
+    // def add
     public static BigInteger add(BigInteger s1, BigInteger s2) {
         // var sum := s1
         BigInteger sum = s1.add(BigInteger.ZERO);
@@ -35,6 +49,7 @@ public class Test {
         return sum;
     }
 
+    // def sub
     public static BigInteger sub(BigInteger s1, BigInteger s2) {
 
         // var retVal := s1
@@ -52,12 +67,15 @@ public class Test {
         return retVal;
     }
 
+    // def multiply
     public static BigInteger mutliply(BigInteger s1, BigInteger s2) {
         // var retVal := s1
         BigInteger retVal = s1.add(BigInteger.ZERO);
 
         // pred(s2)
         s2 = pred(s2);
+
+        // loop(s2)
         for (BigInteger i = s2.add(BigInteger.ZERO); i.compareTo(BigInteger.ZERO) == 1; i = i
                 .subtract(BigInteger.ONE)) {
 
@@ -69,93 +87,87 @@ public class Test {
         return retVal;
     }
 
+    // def equal
     public static BigInteger equal(BigInteger s1, BigInteger s2) {
         // var retVal := 0
         BigInteger retVal = BigInteger.valueOf(0);
 
+        // var comp1 := sub(s1, s2)
         BigInteger comp1 = sub(s1, s2);
+        // var help1 := isZero(comp1)
         BigInteger help1 = isZero(comp1);
 
         // while(help1)
         while (help1.compareTo(BigInteger.ZERO) != 0) {
 
+            // help1 := 0
             help1 = BigInteger.valueOf(0);
+
+            // var comp2 := sub(s2, s1)
             BigInteger comp2 = sub(s2, s1);
+            // var help2 := sub(comp2)
             BigInteger help2 = isZero(comp2);
 
             // while(help2)
             while (help2.compareTo(BigInteger.ZERO) != 0) {
                 // retVal := 1
                 retVal = BigInteger.valueOf(1);
+                // help2 := 0
                 help2 = BigInteger.valueOf(0);
             }
         }
 
+        // return retVal
         return retVal;
     }
 
+    // def isNotZero
     public static BigInteger isNotZero(BigInteger s1) {
+        // var retVal := 0
         BigInteger retVal = BigInteger.valueOf(0);
 
+        // while (s1)
         while (s1.compareTo(BigInteger.ZERO) != 0) {
+            // retVal := 1
             retVal = BigInteger.valueOf(1);
+            // s1 := 0
             s1 = BigInteger.valueOf(0);
         }
 
+        // return retVal
         return retVal;
     }
 
+    // def isZero
     public static BigInteger isZero(BigInteger s1) {
+        // var retVal := 1
         BigInteger retVal = BigInteger.valueOf(1);
 
+        // while(s1)
         while (s1.compareTo(BigInteger.ZERO) != 0) {
+            // retVal := 0
             retVal = BigInteger.valueOf(0);
+            // s1 := 0
             s1 = BigInteger.valueOf(0);
         }
 
+        // return retVal
         return retVal;
     }
 
+    // Jedes Statemant ausserhalb von Funktionen landet in der Main-Methode
     public static void main(String[] args) {
 
-        BigInteger s1 = BigInteger.valueOf(10);
-        BigInteger s2 = BigInteger.valueOf(20);
+        // var result := 1
+        BigInteger result = BigInteger.valueOf(1);
 
-        System.out.println("Add");
-        System.out.println(add(s1, s2));
-        System.out.println(add(s2, s1));
+        // var r0 = read()
+        BigInteger r0 = read("r0");
 
-        System.out.println("Sub");
-        System.out.println(sub(s1, s2));
-        System.out.println(sub(s2, s1));
+        // result := r0
+        result = r0;
 
-        System.out.println("Multi");
-        System.out.println(mutliply(s1, s2));
-        System.out.println(mutliply(s2, s1));
-
-        System.out.println("Equal");
-        System.out.println(equal(s1, s2));
-        System.out.println(equal(s2, s1));
-        System.out.println(equal(s2, s2));
-
-        System.out.println("IsZero");
-        System.out.println(isZero(s1));
-        System.out.println(isZero(s2));
-
-        System.out.println("IsNotZero");
-        System.out.println(isNotZero(s1));
-        System.out.println(isNotZero(s2));
-
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("R0 = ");
-        BigInteger r0 = new BigInteger(in.nextLine());
-
-        BigInteger result = BigInteger.valueOf(0);
-
-        System.out.println(r0);
-        System.out.println(isZero(r0));
-
-        in.close();
+        // write(r0)
+        System.out.println("Result: " + result);
     }
 }
