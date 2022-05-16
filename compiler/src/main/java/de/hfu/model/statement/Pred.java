2de6d24d1bdcac7c4dd62e29dcd45b7343e1142e
Anton Petrov <petrov.anton@web.de>
2022-05-16 18:46:48 +0200

    Added Pred

--- compiler/src/main/java/de/hfu/model/statement/Pred.java
+++ compiler/src/main/java/de/hfu/model/statement/Pred.java
@@ -1,5 +1,13 @@
 package de.hfu.model.statement;
 
 public class Pred extends Statement {
+    private String varName;
 
+    public Pred(String varName) {
+        this.varName = varName;
+    }
+
+    public String getVarName() {
+        return varName;
+    }
 }
