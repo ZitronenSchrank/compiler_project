5db3007eb8e294382a4eb48a55eb8f1457a3589c
Anton Petrov <48187945+ZitronenSchrank@users.noreply.github.com>
2022-05-19 14:37:47 +0200

    AvailableVariable Is Now Using Stack

--- compiler/src/main/java/de/hfu/util/AvailableVariables.java
+++ compiler/src/main/java/de/hfu/util/AvailableVariables.java
@@ -1,71 +1,87 @@
 package de.hfu.util;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.Stack;
 
 public class AvailableVariables {
-    private List<Set<String>> availableVariables;
-    private List<Set<String>> forbiddenVariables;
+    private Stack<Set<String>> availableVariables;
+    private Stack<Set<String>> forbiddenVariables;
 
     public AvailableVariables() {
-        availableVariables = new ArrayList<>();
-        forbiddenVariables = new ArrayList<>();
+        availableVariables = new Stack<>();
+        forbiddenVariables = new Stack<>();
         pushNewContext();
     }
 
     public void pushNewContext() {
-        availableVariables.add(new HashSet<>());
-        forbiddenVariables.add(new HashSet<>());
+        try {
+            // Copy Every VarName From Old Context Into The New One
+            Set<String> oldAvailable = availableVariables.lastElement();
+            Set<String> oldForbidden = forbiddenVariables.lastElement();
+
+            Set<String> newAvailable = new HashSet<>();
+            Set<String> newForbidden = new HashSet<>();
+
+            for (var s : oldAvailable) {
+                newAvailable.add(s);
+            }
+            for (var s : oldForbidden) {
+                newForbidden.add(s);
+            }
+
+            availableVariables.push(newAvailable);
+            forbiddenVariables.push(newForbidden);
+        } catch (NoSuchElementException e) {
+            availableVariables.push(new HashSet<>());
+            forbiddenVariables.push(new HashSet<>());
+        }
     }
 
     public boolean pop() {
         if (availableVariables.size() <= 1) {
             return false;
         } else {
-            int indexToRemove = availableVariables.size() - 1;
-            availableVariables.remove(indexToRemove);
-            forbiddenVariables.remove(indexToRemove);
+            availableVariables.pop();
+            forbiddenVariables.pop();
             return true;
         }
 
     }
 
     public boolean addAvailableVariable(String variable) {
-        int currentIndex = availableVariables.size() - 1;
-        return availableVariables.get(currentIndex).add(variable);
+        return availableVariables.lastElement().add(variable);
     }
 
     public boolean addForbiddenVariable(String variable) {
-        int currentIndex = availableVariables.size() - 1;
-        return forbiddenVariables.get(currentIndex).add(variable);
+        return forbiddenVariables.lastElement().add(variable);
     }
 
     public boolean availableVariablesContains(String variable) {
-        for (var set : availableVariables) {
-            if (set.contains(variable) == true) {
-                return true;
-            }
+        Set<String> set = availableVariables.lastElement();
+        if (set.contains(variable) == true) {
+            return true;
         }
         return false;
+
     }
 
     public boolean forbiddenVariablesContains(String variable) {
-        for (var set : forbiddenVariables) {
-            if (set.contains(variable) == true) {
-                return true;
-            }
+        Set<String> set = forbiddenVariables.lastElement();
+        if (set.contains(variable) == true) {
+            return true;
         }
         return false;
     }
 
     public boolean contains(String variable) {
-        for (int i = 0; i < availableVariables.size(); i++) {
-            if (availableVariables.get(i).contains(variable) | forbiddenVariables.get(i).contains(variable)) {
-                return true;
-            }
+
+        if (availableVariables.lastElement().contains(variable)
+                | forbiddenVariables.lastElement().contains(variable)) {
+            return true;
         }
+
         return false;
     }
 }
