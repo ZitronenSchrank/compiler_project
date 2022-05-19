2717f2f2a9ce58e0af6cb3a63e6ed90d3ef16770
Richard Hempel <richard.hempel@hs-furtwangen.de>
2022-05-19 15:28:54 +0200

    Added predefined language functionalities and partially the generation of functions

--- compiler/src/main/java/de/hfu/generator/Generator.java
+++ compiler/src/main/java/de/hfu/generator/Generator.java
@@ -9,13 +9,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import static java.nio.file.StandardOpenOption.*;
 
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
-
 import de.hfu.model.DefFunction;
 import de.hfu.model.Program;
 import de.hfu.model.expression.*;
 import de.hfu.model.statement.*;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Handle;
 
 import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
 import static org.objectweb.asm.Opcodes.ACC_STATIC;
@@ -24,6 +27,18 @@ import static org.objectweb.asm.Opcodes.GETSTATIC;
 import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
 import static org.objectweb.asm.Opcodes.V1_8;
 import static org.objectweb.asm.Opcodes.RETURN;
+import static org.objectweb.asm.Opcodes.ARETURN;
+import static org.objectweb.asm.Opcodes.ALOAD;
+import static org.objectweb.asm.Opcodes.ASTORE;
+import static org.objectweb.asm.Opcodes.ICONST_M1;
+import static org.objectweb.asm.Opcodes.IF_ICMPNE;
+import static org.objectweb.asm.Opcodes.NEW;
+import static org.objectweb.asm.Opcodes.DUP;
+import static org.objectweb.asm.Opcodes.INVOKESPECIAL;
+import static org.objectweb.asm.Opcodes.ICONST_0;
+import static org.objectweb.asm.Opcodes.ACC_VARARGS;
+import static org.objectweb.asm.Opcodes.AALOAD;
+import static org.objectweb.asm.Opcodes.GETFIELD;
 
 public class Generator {
 
@@ -60,6 +75,7 @@ public class Generator {
         }
 
         MethodVisitor mainMethodVisitor = classWriter.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+        mainMethodVisitor.visitCode();
         
         for (Statement statement : whileProgram.getStatements()) {
             this.generateStatementCode(mainMethodVisitor, statement);
@@ -77,12 +93,133 @@ public class Generator {
     }
 
     private void generateLanguageFunctionalityCode() {
-        // TODO
+        MethodVisitor methodVisitor;
+        
+        {   //PRED
+            methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_STATIC, "pred", "(Ljava/math/BigInteger;)Ljava/math/BigInteger;", null, null);
+            methodVisitor.visitCode();
+            Label label0 = new Label();
+            methodVisitor.visitLabel(label0);
+            methodVisitor.visitLineNumber(10, label0);
+            methodVisitor.visitVarInsn(ALOAD, 0);
+            methodVisitor.visitFieldInsn(GETSTATIC, "java/math/BigInteger", "ZERO", "Ljava/math/BigInteger;");
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/math/BigInteger", "add", "(Ljava/math/BigInteger;)Ljava/math/BigInteger;", false);
+            methodVisitor.visitVarInsn(ASTORE, 1);
+            Label label1 = new Label();
+            methodVisitor.visitLabel(label1);
+            methodVisitor.visitLineNumber(11, label1);
+            methodVisitor.visitVarInsn(ALOAD, 1);
+            methodVisitor.visitFieldInsn(GETSTATIC, "java/math/BigInteger", "ONE", "Ljava/math/BigInteger;");
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/math/BigInteger", "subtract", "(Ljava/math/BigInteger;)Ljava/math/BigInteger;", false);
+            methodVisitor.visitVarInsn(ASTORE, 1);
+            Label label2 = new Label();
+            methodVisitor.visitLabel(label2);
+            methodVisitor.visitLineNumber(14, label2);
+            methodVisitor.visitVarInsn(ALOAD, 1);
+            methodVisitor.visitFieldInsn(GETSTATIC, "java/math/BigInteger", "ZERO", "Ljava/math/BigInteger;");
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/math/BigInteger", "compareTo", "(Ljava/math/BigInteger;)I", false);
+            methodVisitor.visitInsn(ICONST_M1);
+            Label label3 = new Label();
+            methodVisitor.visitJumpInsn(IF_ICMPNE, label3);
+            Label label4 = new Label();
+            methodVisitor.visitLabel(label4);
+            methodVisitor.visitLineNumber(15, label4);
+            methodVisitor.visitFieldInsn(GETSTATIC, "java/math/BigInteger", "ZERO", "Ljava/math/BigInteger;");
+            methodVisitor.visitVarInsn(ASTORE, 1);
+            methodVisitor.visitLabel(label3);
+            methodVisitor.visitLineNumber(17, label3);
+            methodVisitor.visitFrame(Opcodes.F_APPEND,1, new Object[] {"java/math/BigInteger"}, 0, null);
+            methodVisitor.visitVarInsn(ALOAD, 1);
+            methodVisitor.visitInsn(ARETURN);
+            methodVisitor.visitMaxs(2, 2);
+            methodVisitor.visitEnd();
+        }   //!PRED
+        {   //SUCC
+            methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_STATIC, "succ", "(Ljava/math/BigInteger;)Ljava/math/BigInteger;", null, null);
+            methodVisitor.visitCode();
+            Label label0 = new Label();
+            methodVisitor.visitLabel(label0);
+            methodVisitor.visitLineNumber(22, label0);
+            methodVisitor.visitVarInsn(ALOAD, 0);
+            methodVisitor.visitFieldInsn(GETSTATIC, "java/math/BigInteger", "ONE", "Ljava/math/BigInteger;");
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/math/BigInteger", "add", "(Ljava/math/BigInteger;)Ljava/math/BigInteger;", false);
+            methodVisitor.visitInsn(ARETURN);
+            methodVisitor.visitMaxs(2, 1);
+            methodVisitor.visitEnd();
+        }   //!SUCC
+        {   //READ
+            methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_STATIC, "read", "(Ljava/lang/String;)Ljava/math/BigInteger;", null, null);
+            methodVisitor.visitCode();
+            Label label0 = new Label();
+            methodVisitor.visitLabel(label0);
+            methodVisitor.visitLineNumber(27, label0);
+            methodVisitor.visitTypeInsn(NEW, "java/util/Scanner");
+            methodVisitor.visitInsn(DUP);
+            methodVisitor.visitFieldInsn(GETSTATIC, "java/lang/System", "in", "Ljava/io/InputStream;");
+            methodVisitor.visitMethodInsn(INVOKESPECIAL, "java/util/Scanner", "<init>", "(Ljava/io/InputStream;)V", false);
+            methodVisitor.visitVarInsn(ASTORE, 1);
+            Label label1 = new Label();
+            methodVisitor.visitLabel(label1);
+            methodVisitor.visitLineNumber(28, label1);
+            methodVisitor.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            methodVisitor.visitVarInsn(ALOAD, 0);
+            methodVisitor.visitInvokeDynamicInsn("makeConcatWithConstants", "(Ljava/lang/String;)Ljava/lang/String;", new Handle(Opcodes.H_INVOKESTATIC, "java/lang/invoke/StringConcatFactory", "makeConcatWithConstants", "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/invoke/CallSite;", false), new Object[]{"\u0001 := "});
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
+            Label label2 = new Label();
+            methodVisitor.visitLabel(label2);
+            methodVisitor.visitLineNumber(29, label2);
+            methodVisitor.visitVarInsn(ALOAD, 1);
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/util/Scanner", "nextLine", "()Ljava/lang/String;", false);
+            methodVisitor.visitVarInsn(ASTORE, 2);
+            Label label3 = new Label();
+            methodVisitor.visitLabel(label3);
+            methodVisitor.visitLineNumber(30, label3);
+            methodVisitor.visitVarInsn(ALOAD, 1);
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/util/Scanner", "close", "()V", false);
+            Label label4 = new Label();
+            methodVisitor.visitLabel(label4);
+            methodVisitor.visitLineNumber(31, label4);
+            methodVisitor.visitTypeInsn(NEW, "java/math/BigInteger");
+            methodVisitor.visitInsn(DUP);
+            methodVisitor.visitVarInsn(ALOAD, 2);
+            methodVisitor.visitMethodInsn(INVOKESPECIAL, "java/math/BigInteger", "<init>", "(Ljava/lang/String;)V", false);
+            methodVisitor.visitInsn(ARETURN);
+            methodVisitor.visitMaxs(3, 3);
+            methodVisitor.visitEnd();
+        }   //!READ
+        {   //WRITE
+            methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_STATIC | ACC_VARARGS, "write", "([Lde/hfu/Example$Value;)V", null, null);
+            methodVisitor.visitCode();
+            Label label0 = new Label();
+            methodVisitor.visitLabel(label0);
+            methodVisitor.visitLineNumber(159, label0);
+            methodVisitor.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            methodVisitor.visitVarInsn(ALOAD, 0);
+            methodVisitor.visitInsn(ICONST_0);
+            methodVisitor.visitInsn(AALOAD);
+            methodVisitor.visitFieldInsn(GETFIELD, "de/hfu/Example$Value", "name", "Ljava/lang/String;");
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+            Label label1 = new Label();
+            methodVisitor.visitLabel(label1);
+            methodVisitor.visitLineNumber(160, label1);
+            methodVisitor.visitInsn(RETURN);
+            methodVisitor.visitMaxs(3, 1);
+            methodVisitor.visitEnd();
+        }   //!WRITE
     }
 
     private void generateFunctionCode(DefFunction function) {
-        // TODO
-        System.out.println(function.getId());
+        MethodVisitor currentFunctionVisitor = classWriter.visitMethod(ACC_PUBLIC + ACC_STATIC, function.getId(), "(Ljava/lang/String;)Ljava/math/BigInteger;", null, null);
+        currentFunctionVisitor.visitCode();
+        
+        //TODO Einlesen von Parametern
+
+        for (Statement statement : function.getStatements()) {
+            this.generateStatementCode(currentFunctionVisitor, statement);
+        }
+        currentFunctionVisitor.visitInsn(ARETURN); //TODO Return muss noch getestet werden und verbindung mit Statements muss hergestellt werden (Laden vom Rückgabewert)
+        currentFunctionVisitor.visitEnd();
+        currentFunctionVisitor.visitMaxs(-1,-1);
     }
 
     private void generateStatementCode(MethodVisitor methodVisitor, Statement statement) {
@@ -104,13 +241,6 @@ public class Generator {
             default: System.err.println("Statement: " + statement.getClass().getSimpleName() + "not found!");
             break;
         }
-
-        //TODO
-        System.out.println(statement.getClass().toString());
-        methodVisitor.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-        methodVisitor.visitLdcInsn(statement.getClass().toString());
-        methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
-
     }
 
     private void generateExpressionCode(MethodVisitor methodVisitor, Expression expression){
@@ -134,9 +264,6 @@ public class Generator {
 
     private void generateDefVarCode(MethodVisitor methodVisitor, DefVar defVar) {
         //TODO
-        methodVisitor.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-        methodVisitor.visitLdcInsn(defVar.getVarName() + " := " + ((NumExpression)defVar.getInitValue()).getNumString());
-        methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
     }
 
     private void generateLoopCode(MethodVisitor methodVisitor, Loop statement) {
